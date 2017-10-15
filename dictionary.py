a363330534d109e570261e4d6319416f6d268bcb
sam2802 <samu2802@gmail.com>
2017-10-15 15:10:40 -0500

    Added logic for spell check

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,5 @@
 import json
-
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -7,7 +7,11 @@ def translate(word):
     if word in data:
         return data[word]
     else :
-        return "The word doesn't exist , Please double check"
+        w=get_close_matches(word,data.keys())[0]
+        if w.__len__() > 0:
+            return "Did you mean " + w
+        else :
+            return "The word doesn't exist , Please double check"
 
 
 word = input("Enter a word: ").lower()
