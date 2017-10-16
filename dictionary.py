c20726cfa21bff7ab1ddac46f17e5fa77a939185
sam2802 <samu2802@gmail.com>
2017-10-15 20:20:05 -0500

    proper nouns bug fixed

--- dictionary.py
+++ dictionary.py
@@ -4,15 +4,19 @@ from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def translate(word):
-    word = word.lower()
+    word = word.capitalize()
     if word in data:
         return data[word]
+    elif w.lower() in data:
+        return data[w.lower()]
     elif get_close_matches(word,data.keys(),cutoff=0.8).__len__()> 0:
         conf = input("Did you mean '" + get_close_matches(word,data.keys(),cutoff=0.8)[0] +"' instead? (Type y or n) : ")
         if conf.lower() == 'y':
             return data[get_close_matches(word,data.keys(),cutoff=0.8)[0]]
-        else :
-            exit()
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
     else :
         return "The word doesn't exist , Please double check"
 
